3dc193968f99b1a0384c7f03b403b33039726c06
Lakshmi141105 <79718009+Lakshmi141105@users.noreply.github.com>
2021-03-06 21:41:27 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,249 @@
+
+  <?php require "header.php" ?>
+
+
+  <main id="main">
+  <?php require "inner-home.php" ?>
+ <?php require "testimonials.php"?>
+
+    <!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Portfolio</h2>
+          <p>Welcome to the Portfolio. Here you can find awesome products and striking services for the art lovers.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-12 d-flex justify-content-center">
+            <ul id="portfolio-flters">
+              <li data-filter="*" class="filter-active">All</li>
+              <li data-filter=".filter-product">Products</li>
+              <li data-filter=".filter-service">Services</li>             
+            </ul>
+          </div>
+        </div>
+
+        <div class="row portfolio-container">
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-product">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/oilpainting-1.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Product 1</h4>
+                <p>Oil Painting</p>
+              </div>
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/oilpainting-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title=">Product 1"><i class="bx bx-plus"></i></a>
+                <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-service">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/rain-1.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Service 1</h4>
+                <p>Energetic</p>
+              </div>
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/oilpainting-4.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Service 1"><i class="bx bx-plus"></i></a>
+                <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-product">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/oilpainting-4.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Product 2</h4>
+                <p>oil painting</p>
+              </div>
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/oilpainting-4.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Product 2"><i class="bx bx-plus"></i></a>
+                <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-product">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/oilpainting-3.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Product 3</h4>
+                <p>oil painting</p>
+              </div>
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/oilpainting-3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Product 3"><i class="bx bx-plus"></i></a>
+                <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-service">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/happy-1.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Service 3</h4>
+                <p>jubilant</p>
+              </div>
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/happy-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Service 3"><i class="bx bx-plus"></i></a>
+                <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-service">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/emotional.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Service 4</h4>
+                <p>Emotional</p>
+              </div>
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/emotional.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Service 4"><i class="bx bx-plus"></i></a>
+                <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-product">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/acrylic-1.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Product 4</h4>
+                <p>Acrylic painting</p>
+              </div>
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/acrylic-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Product 4"><i class="bx bx-plus"></i></a>
+                <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-product">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/acrylic-2.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Product 5</h4>
+                <p>Acrylic painting</p>
+              </div>
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/acrylic-2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Product 5"><i class="bx bx-plus"></i></a>
+                <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-service">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/thoughtful.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Service 5</h4>
+                <p>Thoughful</p>
+              </div>
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/thoughtful.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Service 5"><i class="bx bx-plus"></i></a>
+                <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Section -->
+
+    <!-- ======= Pricing Section ======= -->
+    <section id="pricing" class="pricing section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Pricing</h2>
+          <p>Choose a plan thats right for you! .</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6" data-aos="fade-up" data-aos-delay="100">
+            <div class="box">
+              <h3>Free</h3>
+              <h4><sup>$</sup>0<span> / month</span></h4>
+              <ul>
+                <li>Download images</li>
+                <li>Basic moods paintings</li>
+                <li>Raw paintings</li>
+                <li class="na">Deep Emotion Depictions</li>
+                <li class="na">Refined Paintings</li>
+              </ul>
+              <div class="btn-wrap">
+                <a href="#" class="btn-buy">Buy Now</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mt-4 mt-md-0" data-aos="fade-up" data-aos-delay="200">
+            <div class="box featured">
+              <h3>Essential</h3>
+              <h4><sup>$</sup>100<span> / month</span></h4>
+              <ul>
+              <li>Download images</li>
+                <li>Basic mood Depictions</li>
+                <li>Raw paintings</li>
+                <li>Deep Emotion Depictions</li>
+                <li class="na">Refined Paintings</li>
+              </ul>
+              <div class="btn-wrap">
+                <a href="#" class="btn-buy">Buy Now</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="300">
+            <div class="box">
+              <h3>Professional</h3>
+              <h4><sup>$</sup>599<span> / month</span></h4>
+              <ul>
+              <li>Download images</li>
+                <li>Basic mood Depictions</li>
+                <li>Raw paintings</li>
+                <li>Deep Emotion Depictions</li>
+                <li>Refined Paintings</li>
+              </ul>
+              <div class="btn-wrap">
+                <a href="#" class="btn-buy">Buy Now</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="400">
+            <div class="box">
+              <span class="advanced">Advanced</span>
+              <h3>Ultimate</h3>
+              <h4><sup>$</sup>1499<span> / month</span></h4>
+              <ul>
+              <li>Download images</li>
+                <li>Basic mood Depictions</li>
+                <li>Raw paintings</li>
+                <li>Deep Emotion Depictions</li>
+                <li>Refined Paintings</li>
+              </ul>
+              <div class="btn-wrap">
+                <a href="#" class="btn-buy">Buy Now</a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Pricing Section -->
+
+  </main><!-- End #main -->
+
+
+  <?php require "footer.php" ?>
